84a07d67616cb5ae9ef3ee8c5b6167507da1cd02
emptyinside1 <morevdaniil162@gmail.com>
2026-03-04 08:55:55 +0300

    Правки nix-ld

--- sys-pkgs.nix
+++ sys-pkgs.nix
@@ -65,6 +65,39 @@
     mesa
     alsa-lib
     fontconfig
-    # Добавьте другие, если Dotify будет ругаться на отсутствие .so файлов
+    # --- Дополнительные пакеты для nix-ld.libraries ---
+    stdenv.cc.cc       # Стандартная библиотека C++ (libstdc++.so.6)
+    zlib               # Сжатие данных
+    fuse3              # Для запуска AppImage без распаковки
+    icu                # Поддержка разных кодировок и языков
+    libxkbcommon       # Раскладки клавиатуры
+    libsecret          # Хранение паролей (часто просит Electron)
+    dbus               # Межпроцессное общение
+    expat              # XML парсер
+    systemd            # Библиотека libudev.so.1
+    libxml2            # Еще один XML парсер
+    curl               # Сетевые запросы
+    openssl            # Шифрование и SSL
+    
+    # Мультимедиа и звук
+    libpulseaudio      # Звук (PulseAudio/Pipewire)
+    pipewire           # Современный звук
+    libvorbis          # Аудио-кодеки
+  
+    # Дополнительные X11 и Wayland зависимости
+    xorg.libXrandr
+    xorg.libXcomposite
+    xorg.libXinerama
+    wayland            # Если используете GNOME/KDE на Wayland
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXi
+    xorg.libXrender
+    xorg.libXtst
+    libGL
+      # Добавьте другие, если Dotify будет ругаться на отсутствие .so файлов
   ];
 }
