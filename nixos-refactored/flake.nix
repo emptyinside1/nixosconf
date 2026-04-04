10fd53c7f8c9d652dbb35f7b55e8471c53e94767
emptyinside1 <morevdaniil162@gmail.com>
2026-04-04 21:48:25 +0300

    nixos refactored

--- nixos-refactored/flake.nix
+++ nixos-refactored/flake.nix
@@ -29,6 +29,17 @@
     pkgs-stable = nixpkgs-stable.legacyPackages.${system};
   in
   {
+    nixConfig = {
+      extra-substituters = [
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+
     # Конфигурация для машины с именем "nixos"
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       inherit system;
