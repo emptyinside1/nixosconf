5b1a401f2753e9edc7baccf80dd7e584118cc27f
emptyinside1 <morevdaniil162@gmail.com>
2026-06-13 03:57:55 +0300

    fix: restore tailscale, ssh config, and verify lost features

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -15,7 +15,20 @@
   # Enable Avahi
   services.avahi.enable = true;
 
-  # Firewall configuration
+  # Firewall configuration (Syncthing)
   networking.firewall.allowedTCPPorts = [ 8384 22000 47984 47985 47986 47987 47988 47989 47990 48010 ];
   networking.firewall.allowedUDPPorts = [ 22000 21027 5353 1900 47998 47999 48000 48010 ];
+
+  # Включаем программу Tailscale (это наш невидимый интернет-кабель)
+  services.tailscale.enable = true;
+
+  # Настраиваем подключение к ноутбуку
+  programs.ssh.extraConfig = ''
+    Host homeserver
+      # Пока что тут пишем локальный IP ноутбука (узнай его на роутере, обычно начинается на 192.168...)
+      HostName 100.112.148.70
+      User admin
+      IdentityFile ~/.ssh/id_ed25519 # Это путь к твоему ключу-паролю
+      StrictHostKeyChecking accept-new # Чтобы комп не ругался на новые подключения
+  '';
 }
