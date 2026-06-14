26600c32affaaeea599169d79d8b3ad0c42c3657
emptyinside1 <morevdaniil162@gmail.com>
2026-06-14 23:55:58 +0300

    ae: fix zapret preStart script by adding -n flag to ln -sf

--- modules/nixos/zapret.nix
+++ modules/nixos/zapret.nix
@@ -32,20 +32,27 @@
       ipset
       which
       kmod
-      shadow # Added for useradd
+      shadow
     ];
 
     preStart = ''
-      mkdir -p /opt/zapret
+      mkdir -p /opt/zapret/init.d/sysv
+      
+      # Copy configuration
       cp -f /etc/zapret.conf /opt/zapret/config
       
-      ln -sf ${pkgs.zapret}/usr/share/zapret/common /opt/zapret/common
-      ln -sf ${pkgs.zapret}/usr/share/zapret/ipset /opt/zapret/ipset
-      ln -sf ${pkgs.zapret}/usr/share/zapret/files /opt/zapret/files
-      mkdir -p /opt/zapret/init.d/sysv
-      ln -sf ${pkgs.zapret}/usr/share/zapret/functions /opt/zapret/init.d/sysv/functions
-      ln -sf ${pkgs.zapret}/bin/zapret /opt/zapret/init.d/sysv/zapret
+      # Link necessary directories from nix store to /opt/zapret
+      # Using -n (no-dereference) is CRITICAL here to prevent ln from 
+      # trying to create links INSIDE the target directories on second run
+      ln -sfn ${pkgs.zapret}/usr/share/zapret/common /opt/zapret/common
+      ln -sfn ${pkgs.zapret}/usr/share/zapret/ipset /opt/zapret/ipset
+      ln -sfn ${pkgs.zapret}/usr/share/zapret/files /opt/zapret/files
+      
+      # Correct path for functions and script
+      ln -sfn ${pkgs.zapret}/usr/share/zapret/init.d/sysv/functions /opt/zapret/init.d/sysv/functions
+      ln -sfn ${pkgs.zapret}/bin/zapret /opt/zapret/init.d/sysv/zapret
 
+      # Create empty lists if they don't exist
       touch /opt/zapret/ipset-exclude.txt
       touch /opt/zapret/ipset-whitelist.txt
       touch /opt/zapret/hostlist.txt
@@ -60,26 +67,25 @@
     
     FWTYPE="iptables"
     
-    # Enable NFQWS explicitly
+    # Enable NFQWS
     NFQWS_ENABLE=1
     TPWS_ENABLE=0
     
     MODE="nfqws"
     MODE_FILTER="none"
     
-    # Strategy from blockcheck
+    # Strategy from blockcheck (Confirmed working for you)
     NFQWS_OPT="--dpi-desync=fake --dpi-desync-ttl=6"
     
     NFQWS_PORTS_TCP="80,443"
     NFQWS_PORTS_UDP="443"
     
-    # NixOS already has 'nobody' user
     WS_USER="nobody"
-    
     DISABLE_IPV6=0
     INIT_APPLY_FW=1
     PIDDIR="/opt/zapret"
     
+    # Absolute paths to binaries
     NFQWS="${pkgs.zapret}/bin/nfqws"
     TPWS="${pkgs.zapret}/bin/tpws"
     IPSET_CR="$ZAPRET_BASE/ipset/create_ipset.sh"
