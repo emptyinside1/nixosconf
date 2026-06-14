d9d4d5d9bd9749e07e355e7c0c755c9e4fe522a3
emptyinside1 <morevdaniil162@gmail.com>
2026-06-14 23:31:18 +0300

    ae: update zapret.nix with NFQWS_ENABLE, NFQWS_OPT, and shadow pkg

--- modules/nixos/zapret.nix
+++ modules/nixos/zapret.nix
@@ -32,18 +32,18 @@
       ipset
       which
       kmod
+      shadow # Added for useradd
     ];
 
     preStart = ''
       mkdir -p /opt/zapret
       cp -f /etc/zapret.conf /opt/zapret/config
       
-      # Create necessary structure for the scripts to work
       ln -sf ${pkgs.zapret}/usr/share/zapret/common /opt/zapret/common
       ln -sf ${pkgs.zapret}/usr/share/zapret/ipset /opt/zapret/ipset
       ln -sf ${pkgs.zapret}/usr/share/zapret/files /opt/zapret/files
       mkdir -p /opt/zapret/init.d/sysv
-      ln -sf ${pkgs.zapret}/usr/share/zapret/init.d/sysv/functions /opt/zapret/init.d/sysv/functions
+      ln -sf ${pkgs.zapret}/usr/share/zapret/functions /opt/zapret/init.d/sysv/functions
       ln -sf ${pkgs.zapret}/bin/zapret /opt/zapret/init.d/sysv/zapret
 
       touch /opt/zapret/ipset-exclude.txt
@@ -58,36 +58,28 @@
     ZAPRET_BASE="${pkgs.zapret}/usr/share/zapret"
     ZAPRET_RW="/opt/zapret"
     
-    # Using iptables as verified
     FWTYPE="iptables"
     
-    # Mode: nfqws or tpws
-    MODE="nfqws"
+    # Enable NFQWS explicitly
+    NFQWS_ENABLE=1
+    TPWS_ENABLE=0
     
-    # Filter mode: none, ipset, hostlist, autohostlist
+    MODE="nfqws"
     MODE_FILTER="none"
     
-    # NFQWS parameters - UPDATED based on your blockcheck results
-    # Strategy that worked for HTTPS: --dpi-desync=fake --dpi-desync-ttl=6
-    NFQWS_OPT_DESYNC="--dpi-desync=fake --dpi-desync-ttl=6"
+    # Strategy from blockcheck
+    NFQWS_OPT="--dpi-desync=fake --dpi-desync-ttl=6"
     
-    # Ports to intercept
     NFQWS_PORTS_TCP="80,443"
     NFQWS_PORTS_UDP="443"
     
-    # User to run daemons
+    # NixOS already has 'nobody' user
     WS_USER="nobody"
     
-    # IP versions
     DISABLE_IPV6=0
-    
-    # Firewall settings
     INIT_APPLY_FW=1
-    
-    # PID directory
     PIDDIR="/opt/zapret"
     
-    # Ensure binary paths
     NFQWS="${pkgs.zapret}/bin/nfqws"
     TPWS="${pkgs.zapret}/bin/tpws"
     IPSET_CR="$ZAPRET_BASE/ipset/create_ipset.sh"
